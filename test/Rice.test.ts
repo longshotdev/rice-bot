e0f954ba9e203824b950d157b863aa79e3487915
AndyIsCool5463 <andyl5463@gmail.com>
2020-06-09 21:11:49 -0700

    k. work on this later.

--- test/Rice.test.ts
+++ test/Rice.test.ts
@@ -1,18 +1,18 @@
-import unknownTest, { TestInterface } from "ava";
-import Rice from "../src/Rice";
-import createClient from "./helpers/createClient";
-require("dotenv").config();
-const ava = unknownTest as TestInterface<{
-  client: Rice;
-}>;
-ava.before(
-  async (test): Promise<void> => {
-    const client = createClient(); // exported via travis
-    test.context = {
-      client,
-    };
-  }
-);
-ava("choke on my dick", (test): void => {
-  test.pass();
-});
+// import unknownTest, { TestInterface } from "ava";
+// import Rice from "../src/Rice";
+// import createClient from "./helpers/createClient";
+// require("dotenv").config();
+// const ava = unknownTest as TestInterface<{
+//   client: Rice;
+// }>;
+// ava.before(
+//   async (test): Promise<void> => {
+//     const client = createClient(); // exported via travis
+//     test.context = {
+//       client,
+//     };
+//   }
+// );
+// ava("choke on my dick", (test): void => {
+//   test.pass();
+// });

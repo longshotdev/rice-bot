37d464d4a3d3b2ebacfeeb0048aee65ca9561612
AndyIsCool5463 <andyl5463@gmail.com>
2020-06-09 21:13:55 -0700

    ...... final commit - phusnign into the shit

--- test/helpers/createClient.ts
+++
@@ -1,7 +0,0 @@
-import Rice from "../../src/Rice";
-
-export default function createClient(): Rice {
-  const client = Rice.getInstance(); // this creates it lmfao.
-  client.login(process.env.DISCORD_TOKEN);
-  return client;
-}

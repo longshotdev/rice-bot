d3cb344a8cd67df280b9734847a4671b9a765329
AndyIsCool5463 <andyl5463@gmail.com>
2020-07-01 19:23:26 -0700

    I think its a good time to push

--- src/core/Argument.ts
+++ src/core/Argument.ts
@@ -1,29 +1,28 @@
+// const open = ['[', '(', '<'];
+// const close = [']', ')', '>'];
+// const space = [' ', '\n'];
 
-const open = ['[', '(', '<'];
-const close = [']', ')', '>'];
-const space = [' ', '\n'];
+// class Usage {
 
-class Usage {
-    
-}
+// }
 
-} 
-interface UsageContext {
-    tags: Tag[];
-    opened: number;
-    current: string;
-    openRegex: boolean;
-    openReq: TagRequirement;
-    last: boolean;
-    char: number;
-    from: number;
-    at: string;
-    fromTo: string;
-}
-export const enum TagRequirement {
-    Optional,
-    SemiRequired,
-    Required
-}
+// }
+// interface UsageContext {
+//     tags: Tag[];
+//     opened: number;
+//     current: string;
+//     openRegex: boolean;
+//     openReq: TagRequirement;
+//     last: boolean;
+//     char: number;
+//     from: number;
+//     at: string;
+//     fromTo: string;
+// }
+// export const enum TagRequirement {
+//     Optional,
+//     SemiRequired,
+//     Required
+// }
 
-export default Usage;
+// export default Usage;
